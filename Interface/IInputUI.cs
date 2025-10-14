00e07060f27aa9a9613fbb3272a8339cc6c6273a
swott <swot26@gmail.com>
2025-10-14 17:50:05 +0200

    Renormalized line endings?

--- Interface/IInputUI.cs
+++
@@ -1,9 +0,0 @@
-namespace Adventure
-{
-    internal interface IInputUI
-    {
-        string GetInput(string prompt);
-
-        string SelectOption(string heading, string[] options, bool numbersOnly = false);
-    }
-}
